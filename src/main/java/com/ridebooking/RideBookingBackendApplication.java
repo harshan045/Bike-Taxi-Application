3bf8b62c3e0d8d51f540fd72ba6a84801bce90a5
harshan045 <sreeharshan.cen9@gmail.com>
2026-06-29 11:49:23 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/ridebooking/RideBookingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ridebooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideBookingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideBookingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ridebooking/RideBookingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ridebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideBookingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
